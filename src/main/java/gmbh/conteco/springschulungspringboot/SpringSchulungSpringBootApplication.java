9ef0b00d0685780b55e90166b945d941d93448da
Viktor Reichert <viktor.reichert@conteco.gmbh>
2023-01-09 15:47:32 +0100

    ignore Idea

---
+++ src/main/java/gmbh/conteco/springschulungspringboot/SpringSchulungSpringBootApplication.java
@@ -0,0 +1,13 @@
+package gmbh.conteco.springschulungspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSchulungSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSchulungSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gmbh/conteco/springschulungspringboot/SpringSchulungSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package gmbh.conteco.springschulungspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSchulungSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

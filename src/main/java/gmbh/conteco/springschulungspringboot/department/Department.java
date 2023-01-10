2030d3d06d4ab9d3050be7ce52bfbab4c7a61460
Viktor Reichert <viktor.reichert@conteco.gmbh>
2023-01-10 09:36:15 +0100

    Felder setzen

--- src/main/java/gmbh/conteco/springschulungspringboot/department/Department.java
+++ src/main/java/gmbh/conteco/springschulungspringboot/department/Department.java
@@ -1,12 +1,26 @@
 package gmbh.conteco.springschulungspringboot.department;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
 public class Department {
     @Id
-    private Long id;
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "dep_id", nullable = false)
+    private Long department_id;
 
+    @Column(name = "name", nullable = false)
     private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getDepartment_id() {
+        return department_id;
+    }
 }

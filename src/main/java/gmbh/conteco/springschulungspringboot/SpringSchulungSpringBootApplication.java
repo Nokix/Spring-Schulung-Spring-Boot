cd8965dda70561d1d93937d1e7dedc9d4e02b530
Viktor Reichert <viktor.reichert@conteco.gmbh>
2023-01-10 09:49:09 +0100

    DepartmentRepository & CommandLineRunner

--- src/main/java/gmbh/conteco/springschulungspringboot/SpringSchulungSpringBootApplication.java
+++ src/main/java/gmbh/conteco/springschulungspringboot/SpringSchulungSpringBootApplication.java
@@ -1,13 +1,23 @@
 package gmbh.conteco.springschulungspringboot;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringSchulungSpringBootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringSchulungSpringBootApplication.class, args);
+		System.out.println("Hallo DB!");
+	}
+
+	@Bean
+	CommandLineRunner myApp() {
+		return args -> {
+			System.out.println("Hallo Welt!");
+		};
 	}
 
 }
